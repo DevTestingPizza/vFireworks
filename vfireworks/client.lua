d80322f9a5f4969e3ebc4fa8878d75c35cdab986
Tom Grobbe <31419184+TomGrobbe@users.noreply.github.com>
2017-10-19 19:08:38 +0200

    Update client.lua

--- vfireworks/client.lua
+++ vfireworks/client.lua
@@ -1,14 +1,16 @@
 Citizen.CreateThread(function()
-    if not HasNamedPtfxAssetLoaded("scr_indep_fireworks") then
-        RequestNamedPtfxAsset("scr_indep_fireworks")
-        while not HasNamedPtfxAssetLoaded("scr_indep_fireworks") do
+    local asset1 = "scr_indep_fireworks"
+    if not HasNamedPtfxAssetLoaded(asset) then
+        RequestNamedPtfxAsset(asset)
+        while not HasNamedPtfxAssetLoaded(asset) do
             Citizen.Wait(1)
         end
         print('done')
     end
-    if not HasNamedPtfxAssetLoaded("proj_xmas_firework") then
-        RequestNamedPtfxAsset("proj_xmas_firework")
-        while not HasNamedPtfxAssetLoaded("proj_xmas_firework") do
+    local asset2 = "proj_xmas_firework"
+    if not HasNamedPtfxAssetLoaded(asset2) then
+        RequestNamedPtfxAsset(asset2)
+        while not HasNamedPtfxAssetLoaded(asset2) do
             Citizen.Wait(1)
         end
         print('done')
@@ -16,20 +18,20 @@ Citizen.CreateThread(function()
     while true do
         Citizen.Wait(math.random() * 1000)
         --if IsControlPressed(0, 21) then
-            UseParticleFxAssetNextCall("proj_xmas_firework")
+            UseParticleFxAssetNextCall(asset2)
             local part = StartParticleFxNonLoopedAtCoord("scr_firework_xmas_repeat_burst_rgw", 59.0+ (math.random() * 25), 7223.0 + (math.random() * 25), 32.5, 0.0, 0.0, 0.0, math.random() * 0.3 + 0.5, false, false, false, false)
-            UseParticleFxAssetNextCall("scr_indep_fireworks")
+            UseParticleFxAssetNextCall(asset1)
             local part = StartParticleFxNonLoopedAtCoord("scr_indep_firework_shotburst", 59.0+ (math.random() * 25), 7223.0 + (math.random() * 25), 2.5, 0.0, 0.0, 0.0, math.random() * 1.0 + 1.0, false, false, false, false)
             Citizen.Wait(math.random()*500)
-            UseParticleFxAssetNextCall("proj_xmas_firework")
+            UseParticleFxAssetNextCall(asset2)
             local part = StartParticleFxNonLoopedAtCoord("scr_firework_xmas_spiral_burst_rgw", 59.0+ (math.random() * 25), 7223.0 + (math.random() * 25), 23.5, 0.0, 0.0, 0.0, math.random() * 0.3 + 0.5, false, false, false, false)
-            UseParticleFxAssetNextCall("scr_indep_fireworks")
+            UseParticleFxAssetNextCall(asset1)
             local part = StartParticleFxNonLoopedAtCoord("scr_indep_firework_trailburst", 59.0+ (math.random() * 25), 7223.0 + (math.random() * 25), 2.5, 0.0, 0.0, 0.0, math.random() * 1.0 + 1.0, false, false, false, false)
             Citizen.Wait(math.random()*500)
-            UseParticleFxAssetNextCall("scr_indep_fireworks")
+            UseParticleFxAssetNextCall(asset1)
             local part = StartParticleFxNonLoopedAtCoord("scr_indep_firework_trailburst", 59.0+ (math.random() * 25), 7223.0 + (math.random() * 25), 2.5, 0.0, 0.0, 0.0, math.random() * 1.0 + 1.0, false, false, false, false)
             Citizen.Wait(math.random()*500)
-            UseParticleFxAssetNextCall("scr_indep_fireworks")
+            UseParticleFxAssetNextCall(asset1)
             local part = StartParticleFxNonLoopedAtCoord("scr_indep_firework_shotburst", 59.0+ (math.random() * 25), 7223.0 + (math.random() * 25), 2.5, 0.0, 0.0, 0.0, math.random() * 1.0 +1.0, false, false, false, false)
         --end
     end
